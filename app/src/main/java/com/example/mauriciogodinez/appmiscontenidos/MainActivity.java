c8d7cbb0dab5068033bebfbf89e89b301bc0ede2
Mauricio Godinez <mgodinezp@gmail.com>
2019-12-08 12:54:00 -0600

    optimizacion de variables locales y globales mejor manejo de peticion de permisos en la app se mejora la descripcion de los metodos para que sean entendibles a los devs se hace upgrade al build de gradle y las dependencias

--- app/src/main/java/com/example/mauriciogodinez/appmiscontenidos/MainActivity.java
+++ app/src/main/java/com/example/mauriciogodinez/appmiscontenidos/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.mauriciogodinez.appmiscontenidos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.annotation.TargetApi;
-import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
@@ -11,94 +10,85 @@ import android.provider.CallLog;
 import android.telecom.Call;
 import android.util.Log;
 import android.widget.TextView;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "ContentPLlamadas";
+
     //Creamos la URI del ContentProvider que utilizaremos
-    Uri uriLlamada = CallLog.Calls.CONTENT_URI;
-    //Indicamos los Index para las columnas que nos regresará nuestro cursor al consultar el CP.
-    int numero_i;
-    int fecha_i;
-    int tipo_i;
-    int duracion_i;
-    int geocode_i;
-    //Aqui almacenamos los valores que regrese la consulta del CP.
-    String numero;
-    Long fecha;
-    int tipo;
-    String duracion;
-    String geocode;
-    String tipoLlamada;
-    //en el arreglo indicamos las columnas que regresará en el query del Content Resolver
-    String[] campos = {
+    private Uri uriLlamada = CallLog.Calls.CONTENT_URI;
+
+    //en el arreglo indicamos las columnas que regresará en el query del ContentResolver
+    private String[] colContentResolver = {
             CallLog.Calls.NUMBER,
             CallLog.Calls.DATE,
             CallLog.Calls.TYPE,
             CallLog.Calls.DURATION,
             CallLog.Calls.GEOCODED_LOCATION,
     };
+
     //Es el TextView que guardará todos los datos que obtengamos de la consulta.
-    TextView tvLlamadas;
+    private TextView tvLlamadas;
 
     @TargetApi(Build.VERSION_CODES.M)
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            tvLlamadas = (TextView) findViewById(R.id.tvLlamadas);
-            tvLlamadas.setText("");
-        }
+        tvLlamadas = findViewById(R.id.tvLlamadas);
+        tvLlamadas.setText("");
+    }
 
-        @Override
-        protected void onStart(){
-            super.onStart();
-            tvLlamadas = (TextView) findViewById(R.id.tvLlamadas);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        tvLlamadas = findViewById(R.id.tvLlamadas);
 
-            try{
-                ContentResolver contentResolver = getContentResolver();
-                //Consultamos con la URI especificada y que los regrese ordenados DESC con ref Calls.DATE
-                Cursor registros = contentResolver.query(uriLlamada, campos, null, null,
-                                                                    CallLog.Calls.DATE + " DESC");
-                //Creamos el Formatter para obtener una fecha legible
-                SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm");
-                //Con la matriz del cursos, hacemos un recorrido para que regrese los datos indicados
-                while(registros.moveToNext()){
+        try {
+            //Indicamos los Index para las columnas que nos regresará nuestro cursor
+            // al consultar el ContentProvider.
+            int numero_index, fecha_index, tipo_index, duracion_index, geocode_index;
+
+            //Aqui almacenamos los valores que regrese la consulta del ContentProvider.
+            String numero;
+            long fecha;
+            int tipo;
+            String duracion, geocode, tipoLlamada;
+
+            //Consultamos con la URI especificada para que los regrese
+            // ordenados DESC con ref Calls.DATE
+            Cursor registros = getContentResolver().query(uriLlamada, colContentResolver, null, null,
+                    CallLog.Calls.DATE + " DESC");
+
+            //Creamos el Formatter para obtener una fecha legible
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm");
+
+            //Con la matriz del cursor, hacemos un recorrido
+            // para que regrese los datos indicados
+            if (registros != null) {
+                while (registros.moveToNext()) {
                     //Obtengo los indices de las columnas
-                    numero_i = registros.getColumnIndex(campos[0]);
-                    fecha_i = registros.getColumnIndex(campos[1]);
-                    tipo_i = registros.getColumnIndex(campos[2]);
-                    duracion_i = registros.getColumnIndex(campos[3]);
-                    geocode_i = registros.getColumnIndex(campos[4]);
+                    numero_index = registros.getColumnIndex(colContentResolver[0]);
+                    fecha_index = registros.getColumnIndex(colContentResolver[1]);
+                    tipo_index = registros.getColumnIndex(colContentResolver[2]);
+                    duracion_index = registros.getColumnIndex(colContentResolver[3]);
+                    geocode_index = registros.getColumnIndex(colContentResolver[4]);
 
-                    //Obtengo los datos a partir del indice obtenido
-                    numero = registros.getString(numero_i);
-                    fecha = registros.getLong(fecha_i);//Epoch UNIX
-                    tipo = registros.getInt(tipo_i);
-                    duracion = registros.getString(duracion_i);
-                    geocode = registros.getString(geocode_i);
+                    //Obtengo los datos a partir de los indices
+                    numero = registros.getString(numero_index);
+                    fecha = registros.getLong(fecha_index);//Epoch UNIX
+                    tipo = registros.getInt(tipo_index);
+                    duracion = registros.getString(duracion_index);
+                    geocode = registros.getString(geocode_index);
 
                     //Validando tipo de llamada
-                    switch (tipo){
-                        case CallLog.Calls.INCOMING_TYPE:
-                            tipoLlamada = getResources().getString(R.string.entrada);
-                            break;
-                        case CallLog.Calls.MISSED_TYPE:
-                            tipoLlamada = getResources().getString(R.string.perdida);
-                            break;
-                        case CallLog.Calls.OUTGOING_TYPE:
-                            tipoLlamada = getResources().getString(R.string.salida);
-                            break;
-                        case Call.STATE_CONNECTING:
-                            tipoLlamada = getResources().getString(R.string.conectando);
-                            break;
-                        default:
-                            tipoLlamada = getResources().getString(R.string.undefined);
-                    }
+                    tipoLlamada = returnTipoLlamada(tipo);
+
                     //Creamos un string para concatenar los datos y agregarlos al TextView
                     String detalle = getResources().getString(R.string.etiqueta_numero) + numero + "\n"
                             + getResources().getString(R.string.etiqueta_fecha) + formatter.format(new Date(fecha)) + "\n"
@@ -106,13 +96,36 @@ public class MainActivity extends AppCompatActivity {
                             + getResources().getString(R.string.etiqueta_duracion) + duracion + getResources().getString(R.string.segundos) + "\n"
                             + getResources().getString(R.string.etiqueta_geoCode) + geocode + "\n"
                             + getResources().getString(R.string.etiqueta_llamada) + tipoLlamada + "\n\n";
+
                     //Agregamos el string al TextView y continuamos con el bucle
                     tvLlamadas.append(detalle);
                 }
                 //liberamos la memoria del cursor
                 registros.close();
-            }catch(Exception e){
-                Log.v(TAG, e.getMessage());
             }
+        } catch (Exception e) {
+            Log.v(TAG, e.getMessage());
         }
     }
+
+    /**
+     * Método que valida el tipo de llamada que regresa el ContentProvider y asigna
+     * el string correspondiente (entrada, perdida, salida, conectando, no definida)
+     * @param tipo registro CallLog.Calls.TYPE proporcionado por el ContentProvider
+     * @return string asignado al tipo
+     */
+    private String returnTipoLlamada(int tipo){
+        switch (tipo) {
+            case CallLog.Calls.INCOMING_TYPE:
+                return getResources().getString(R.string.entrada);
+            case CallLog.Calls.MISSED_TYPE:
+                return getResources().getString(R.string.perdida);
+            case CallLog.Calls.OUTGOING_TYPE:
+                return getResources().getString(R.string.salida);
+            case Call.STATE_CONNECTING:
+                return getResources().getString(R.string.conectando);
+            default:
+                return getResources().getString(R.string.undefined);
+        }
+    }
+}
--- app/src/main/java/com/example/mauriciogodinez/appmiscontenidos/PortadaActivity.java
+++ app/src/main/java/com/example/mauriciogodinez/appmiscontenidos/PortadaActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,24 +17,29 @@ import android.view.View;
 
 public class PortadaActivity extends AppCompatActivity {
 
-    private static final String TAG = "ContentPLlamadas";
+    private static final String TAG = PortadaActivity.class.getSimpleName();
+
     //Valores en tiempo que tomará nuestra animacion
     private static final int UI_ANIMATION_SHOW_BARS_DELAY = 1500;
     private static final int NEXT_LAYOUT_DELAY_MILLIS = 150;
+
+    //valores para permisos
+    private int readCall, readContacts;
+
     //Handler para manejar los hilos de la animacion
     private final Handler mShowHandler = new Handler();
+
     //CV para modificar los parametros en su visualizacion
     private View mContentView;
 
     @TargetApi(Build.VERSION_CODES.M)
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_portada);
 
         mContentView = findViewById(R.id.fullscreen_activity_portada);
+
         //Aqui modificamos la vista inicial de la Activity para la animacion
         mContentView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -41,37 +47,51 @@ public class PortadaActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
-        //Valores que obtenemos al preguntar si tenemos los permisos para ejecutar la App
-        int readCall = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG);
-        int readContacts = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-        //Aqui preguntamos si tenemos los permisos de ejecución, en caso contrario pregunta en un cuadro de Dialogo
-        try{
-            //Hasta no obtener los permisos no podemos ejecutar la Activity
-            while(readCall != PackageManager.PERMISSION_GRANTED ||
-                    readContacts != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.READ_CALL_LOG,
-                                Manifest.permission.READ_CONTACTS},
-                        1);
-                readCall = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG);
-                readContacts = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-            }
-        }catch (Exception e) {
-            Log.v(TAG, e.getMessage());
-        }
+
+        //Valores donde guarda el estatus de los permisos que obtenemos al preguntar al usuario
+        readCall = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG);
+        readContacts = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //ejecutamos el metodo para la animacion
-        delayedShow(UI_ANIMATION_SHOW_BARS_DELAY);
+        if (readCall != PackageManager.PERMISSION_GRANTED ||
+                readContacts != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CALL_LOG,
+                            Manifest.permission.READ_CONTACTS},
+                    1);
+        } else {
+            //ejecutamos el metodo para la animacion
+            delayedShow(UI_ANIMATION_SHOW_BARS_DELAY);
+            Log.d(TAG, "ejecutamos la animacion");
+        }
     }
+
+    //Aqui preguntamos si tenemos los permisos de ejecución, en caso contrario pregunta en un cuadro de Dialogo
+    //Hasta no obtener los permisos no podemos ejecutar la Activity
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults.length > 0 &&
+                grantResults[0] != PackageManager.PERMISSION_GRANTED ||
+                grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CALL_LOG,
+                            Manifest.permission.READ_CONTACTS},
+                    1);
+        } else {
+            //ejecutamos el metodo para la animacion
+            delayedShow(UI_ANIMATION_SHOW_BARS_DELAY);
+        }
+    }
+
     //anticipa las acciones de otros hilos e inicia el metodo despues del tiempo indicado
     private void delayedShow(int delayMillis) {
         mShowHandler.removeCallbacks(mShowRunnable);
         mShowHandler.postDelayed(mShowRunnable, delayMillis);
     }
+
     //hilo para la animacion
     private final Runnable mShowRunnable = new Runnable() {
         @SuppressLint("InlinedApi")
@@ -81,6 +101,7 @@ public class PortadaActivity extends AppCompatActivity {
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+
             mShowHandler.removeCallbacks(mShowRunnable);
             //Despues del tiempo ejecuta la siguiente tarea
             mShowHandler.postDelayed(mIntentRunnable, NEXT_LAYOUT_DELAY_MILLIS);
